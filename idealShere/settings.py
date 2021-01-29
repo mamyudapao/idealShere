fedc1d04ca8e87079728ff8bf98b380ac113c2ce
mamyudapao <nosikenit@gmail.com>
2021-01-29 13:02:40 +0900

    add a CustomUser model

--- idealShere/settings.py
+++ idealShere/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #サードパーティ
+    'rest_framework',
+
+
+    #自分作成アプリ
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
