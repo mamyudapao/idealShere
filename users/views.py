d57ebf47566577e7a5bf0ab745c75541f6b2098c
mamyudapao <nosikenit@gmail.com>
2021-01-29 13:17:10 +0900

    implemented CustomUserList

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.serializers import CustomUserSerializer
+from users.models import CustomUser
+
+
+class UserList(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
