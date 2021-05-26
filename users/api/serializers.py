0b44d72fce2a14fd7ce01316a1a8ca215ef9799b
mamyudapao <nosikenit@gmail.com>
2021-05-26 09:10:21 +0900

    feat[mypage]: add made_project fields into CustomUserSerializer

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -6,8 +6,13 @@ import posts.api.serializers as postsSerializers
 
 class CustomUserSerializer(serializers.ModelSerializer):
     projects = postsSerializers.PostSerializer(many=True, read_only=True)
+    made_projects = serializers.SerializerMethodField()
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'skill', 'image', 'introduction', 'projects']
+        fields = ['id', 'username', 'skill', 'image', 'introduction', 'projects', 'made_projects']
+
+    def get_made_projects(self, instance):
+        made_projects = Post.objects.filter(user=instance.id)
+        return made_projects.count()
 
 
