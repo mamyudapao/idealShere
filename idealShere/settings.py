fecd0c223c83ac514c7c6513e7438117c531a74f
mamyudapao <nosikenit@gmail.com>
2021-06-04 16:28:49 +0900

    fix: hide important information

--- idealShere/settings.py
+++ idealShere/settings.py
@@ -155,13 +155,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 
 #AWS settings
-AWS_ACCESS_KEY_ID = 'AKIA5X5CNWZK7J4L3WYN'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'ideal-shere'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_FILE_OVERWRITE = False
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+from . import awsSettings
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
